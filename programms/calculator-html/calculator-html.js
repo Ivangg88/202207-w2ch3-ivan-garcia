00ebaa01e9d157cf32609987818f388f9eef213e
Ivan Garcia <ivanggv2@gmail.com>
2022-07-13 22:32:40 +0200

    fixed bug after eslint

--- programms/calculator-html/calculator-html.js
+++ programms/calculator-html/calculator-html.js
@@ -1,128 +1,120 @@
-const numberButton = document.getElementsByName('data-number');
-const opartionButton = document.getElementsByName('data-opera');
-const resultButton = document.getElementsByName('data-igual')[0];
-const deleteButton = document.getElementsByName('data-delete')[0];
-const changeStyleButton = document.getElementsByClassName('change-style')[0];
-const decimalButton = document.getElementsByName('decimal')[0];
+const numberButton = document.getElementsByName("data-number");
+const opartionButton = document.getElementsByName("data-opera");
+const resultButton = document.getElementsByName("data-igual")[0];
+const deleteButton = document.getElementsByName("data-delete")[0];
+const changeStyleButton = document.getElementsByClassName("change-style")[0];
+const decimalButton = document.getElementsByName("decimal")[0];
 const cssSheet = document.getElementById("css");
 let change = true;
-let result = document.getElementById('result')
-let currentOp = '';
-let previousOp = '';
-let operator = undefined;
-let calculated = false;
-let firstNumber = true;
+const result = document.getElementById("result");
+let currentOp = "";
+let previousOp = "";
+let operator;
 
-numberButton.forEach((button) =>{
-    button.addEventListener('click', ()=>{
-        addNumber(button.innerText);   
-    })
-});
+const changeStyleSheet = () => {
+  if (change) {
+    cssSheet.setAttribute("href", "styles2.css");
+    change = false;
+  } else if (!change) {
+    cssSheet.setAttribute("href", "styles.css");
+    change = true;
+  }
+};
 
-opartionButton.forEach((button) =>{
-    button.addEventListener('click', ()=>{
-        selectOperator(button.innerText);   
-    })
-});
+changeStyleButton.addEventListener("click", changeStyleSheet);
 
-resultButton.addEventListener('click', ()=>{
+const calculate = () => {
+  let calc;
+  const previous = previousOp;
+  const current = currentOp;
+  if (Number.isNaN(previous) || Number.isNaN(current)) return;
+  switch (operator) {
+    case "+":
+      calc = parseFloat(previous) + parseFloat(current);
+      break;
+    case "-":
+      calc = previous - current;
+      break;
+    case "x":
+      calc = previous * current;
+      break;
+    case "/":
+      calc = previous / current;
+      break;
+    default:
+      return;
+  }
+
+  currentOp = calc;
+  operator = undefined;
+  previousOp = "";
+};
+
+const selectOperator = (op) => {
+  if (currentOp === "") return;
+  if (previousOp !== "") {
     calculate();
+  }
+  operator = op.toString();
+  previousOp = currentOp;
+  currentOp = "";
+};
+
+const clear = () => {
+  currentOp = "";
+  previousOp = "";
+  operator = undefined;
+};
+
+const displayUpdate = () => {
+  if (currentOp - Math.floor(currentOp) === 0) {
+    result.value = currentOp;
+  } else {
+    result.value = parseFloat(currentOp).toFixed(3);
+  }
+};
+
+const addNumber = (num) => {
+  currentOp = currentOp.toString() + num.toString();
+  displayUpdate();
+};
+
+const addDecimal = () => {
+  const decimalPoint = ".";
+
+  if (currentOp === "") {
+    currentOp = 0;
+  }
+
+  if (currentOp - Math.floor(currentOp) === 0) {
+    currentOp = currentOp.toString() + decimalPoint;
+    parseFloat(currentOp);
     displayUpdate();
-});
-
-deleteButton.addEventListener('click', ()=>{ 
-    clear();
-    displayUpdate();
-});
-
-const addDecimal = () =>{
-    let decimalPoint = ".";
-
-    if(currentOp ===""){
-        currentOp = 0;
-    }
-
-    if(currentOp - Math.floor(currentOp) === 0){
-        currentOp = currentOp.toString() + decimalPoint;
-        parseFloat(currentOp);
-        displayUpdate();
-    }
-
-}
+  }
+};
 
 decimalButton.addEventListener(`click`, addDecimal);
 
-const changeStyleSheet = () =>{
+numberButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    addNumber(button.innerText);
+  });
+});
 
-    if(change){
-    
-        cssSheet.setAttribute('href','styles2.css');
-        change = false
-    }else if(!change){
-        cssSheet.setAttribute('href','styles.css');
-        change = true
-    }
+opartionButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    selectOperator(button.innerText);
+  });
+});
 
-}
+resultButton.addEventListener("click", () => {
+  calculate();
+  displayUpdate();
+});
 
-changeStyleButton.addEventListener('click', changeStyleSheet);
-
-const selectOperator = (op) =>{
-    if(currentOp === '') return;
-    if(previousOp !== ''){
-        calculate();
-    }
-    operator = op.toString();
-    previousOp = currentOp;
-    currentOp = '';
-}
-
- const calculate = ()=>{
-    let calc;
-    const previous = previousOp;
-    const current = currentOp;
-    if(isNaN(previous) || isNaN(current)) return;
-    switch(operator){
-        case '+':
-            calc = parseFloat(previous) + parseFloat(current);
-            break;
-        case '-':
-            calc = previous - current;
-            break;
-        case 'x':
-            calc = previous * current;
-            break;
-        case '/':
-            calc = previous / current;
-            break;
-        default:
-            return;
-    }
-
-    currentOp = calc;
-    operator = undefined;
-    previousOp = '';
-}
-
-const addNumber =(num) =>{
-    currentOp = currentOp.toString() + num.toString();
-    displayUpdate();
-}
-
-const clear = ()=> {
-    currentOp = '';
-    previousOp = '';
-    operator = undefined;
-}
-
-const displayUpdate = () =>{
-    if(currentOp - Math.floor(currentOp) === 0){
-        result.value = currentOp;
-    }else{
-        result.value = parseFloat(currentOp).toFixed(3);
-    }
-}
+deleteButton.addEventListener("click", () => {
+  clear();
+  displayUpdate();
+});
 
 clear();
-
-
