505628d082defcd401a21770735810002328f888
Ivan Garcia <ivanggv2@gmail.com>
2022-07-14 07:01:19 +0200

    refactor code after eslint

--- programms/calculator/calculator.js
+++ programms/calculator/calculator.js
@@ -1,35 +1,31 @@
-//Calculadora simple con prompt
-
 let numResult;
-let numCalc
+let numCalc;
+const num1 = prompt("Insert the first number");
 
-let num1 = prompt("Insert the first number");
-
-if(isNaN(num1)){
-    alert("Error it´s not a number")
-    console.log("Must be a number /n End of programm")}else{
-let num2 = prompt("Insert the second number");
-if(isNaN(num2)){
-    console.log("Square root: " + Math.sqrt(num1));
-    }else{
+if (Number.isNaN(num1)) {
+  alert("Error it´s not a number");
+  console.log("Must be a number /n End of programm");
+} else {
+  const num2 = prompt("Insert the second number");
+  if (Number.isNaN(num2)) {
+    const sqrtResult = Math.sqrt(num1);
+    console.log(`Square root: ${sqrtResult}`);
+  } else {
     numCalc = ["Sum: ", "Rest: ", "Multiplication: ", "Division: "];
-    numResult = [parseFloat(num1)+parseFloat(num2), num1-num2, num1*num2, num1/num2];
+    numResult = [
+      parseFloat(num1) + parseFloat(num2),
+      num1 - num2,
+      num1 * num2,
+      num1 / num2,
+    ];
+  }
+  console.log("Results:");
+
+  for (let i = 0; i < numResult.length; i++) {
+    if (numResult[i] - Math.floor(numResult[i]) === 0) {
+      console.log(numCalc[i] + numResult[i]);
+    } else {
+      console.log(numCalc[i] + parseFloat(numResult[i]).toFixed(3));
     }
-console.log("Results:")
-
-for(let i = 0; i < numResult.length; i++){
-if (numResult[i] - Math.floor(numResult[i]) === 0) {
-    console.log(numCalc[i] + numResult[i]);
-     }else {
-    console.log(numCalc[i] + parseFloat(numResult[i]).toFixed(3));
-        
-    }
+  }
 }
-
-
-    
-}
-
-
-
-   
